9e36a2dc3e2582b235a900371cfb102d415da39a
laure bouelle <johndoe@example.com>
2021-01-28 18:31:24 +0100

    last version

--- src/components/header-nav.vue
+++ src/components/header-nav.vue
@@ -5,25 +5,44 @@
       alt="logo gauche"
       title="logo gauche"
     />
+    
 
     <nav id="liensConnections">
-      <router-link id="articles" to="/articles">Les articles<br />disponibles</router-link>
-      <router-link id ="nouvelArticle" to="/nouvelarticle"
-        >Ecrire<br />un nouvel article</router-link
-      >
-      <div id="profil">
-        <img
-          id="imgPersonn"
-          src="../assets/iconepersonv2.png"
-          alt="logo personnage"
-          title="logo personnage"
-        />
-        <router-link to="/profil">Mon profil</router-link>
-      </div>
-      <router-link v-if="storedData.isAdmin" to="/moderation"
-        >Modération</router-link
-      >
-      <button @click="logout">> Se déconnecter</button>
+      <svg id="burger-menu" viewBox="0 0 100 80" width="25" height="25">
+        <rect y="2" width="100" height="6"></rect>
+        <rect y="35" width="100" height="6"></rect>
+        <rect y="68" width="100" height="6"></rect>
+      </svg>
+      <input type="checkbox" id="burger-menu-checkbox">
+      <ul>
+        <li>
+          <router-link id="articles" to="/articles">Les articles</router-link>
+        </li>
+        <li>
+          <router-link id="nouvelArticle" to="/nouvelarticle"
+            >Nouvel article</router-link
+          >
+        </li>
+        <li>
+          <div id="profil">
+            <img
+              id="imgPersonn"
+              src="../assets/iconepersonv2.png"
+              alt="logo personnage"
+              title="logo personnage"
+            />
+            <router-link id="mon-profil" to="/profil">Mon profil</router-link>
+          </div>
+        </li>
+        <li v-if="storedData.isAdmin">
+          <router-link to="/moderation"
+            >Modération</router-link
+          >
+        </li>
+        <li>
+          <button @click="logout">> Se déconnecter</button>
+        </li>
+      </ul>
     </nav>
   </header>
 </template>
@@ -57,38 +76,63 @@ export default {
   justify-content: space-around;
   height: 102px;
   background-color: rgb(205, 73, 100);
+  box-shadow: 0 2px 4px silver;
+  overflow: hidden;
 }
 
-#entete img {
+#entete > img {
   height: 60px;
   width: 278;
   padding-left: 10px;
   padding-right: 10px;
   padding-top: 15px;
 }
+
 #imgPersonn {
-  width: 20px;
-  height: 15px;
-  padding-bottom: 5px;
+  height: 21px;
+  margin-right: 10px;
 }
+
 #profil {
-  border: grey 2px solid;
   display: flex;
-  flex-direction: column;
-  max-height: 100px;
   align-items: center;
   border-radius: 10%;
 }
-nav {
-  display: flex;
+
+#burger-menu-checkbox {
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  opacity: 0;
+  cursor: pointer;
+  visibility: hidden;
 }
 
-nav > * {
+#burger-menu {
+  border: 1px solid;
+  border-radius: 7px;
+  padding: 5px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  visibility: hidden;
+}
+
+nav > ul {
+  display: flex;
+  list-style-type: none;
+  padding-left: 0;
+}
+
+nav li {
   margin: 10px;
   text-align: center;
   font-weight: bold;
-  
-   background-color: rgb(209, 85, 116);
+  height: 30px;
+
 }
 
 nav div:hover {
@@ -98,34 +142,64 @@ nav div:hover {
 nav button {
   display: flex;
   align-items: center;
-  margin-left: 60px;
   font-size: 15px;
-  
+  background-color: black;
+  color: white;
+  border: 0 transparent;
+  padding: 4px 10px;
+  border-radius: 4px;
+  box-shadow: 1px 1px 6px grey;
 }
-#articles {
-  display: flex;
+#articles, #nouvelArticle, #mon-profil {
   text-align: center;
   font-size: 20px;
-  border: grey 2px solid;
-  border-radius: 10%;
-  
-  align-items: center;
-}
-#nouvelArticle {
-   display: flex;
-  font-size: 20px;
-  border: grey 2px solid;
-  border-radius: 10%;
-   align-items: center;
 }
 
+@media all and (max-width: 840px) {
+  #entete {
+    justify-content: space-between;
+  }
+
+  #burger-menu-checkbox, #burger-menu {
+    visibility: visible;
+  }
+
+  #burger-menu-checkbox:checked + ul {
+    box-shadow: -3px 5px 4px silver; 
+    border-bottom-left-radius: 4px;
+    transition: all 0.2s linear;
+    right: 0;
+  }
+  
+  nav > ul {
+        flex-direction: column;
+        background-color: rgb(205, 73, 100);;
+        padding: 5px;
+        width: 230px;
+        top: 85px;
+        position: absolute;
+        right: -240px;
+        z-index: 2;
+  }
+
+  nav li {
+    border-bottom: 1px solid;
+  }
+
+  nav li:last-child {
+    border-bottom: 0 transparent;
+  }
+
+  nav button {
+    margin-left: 0;
+  }
+}
 
 @media all and (max-width: 624px) {
   nav button {
     display: flex;
     align-items: center;
     margin-right: 5px;
-    background-color: rgb(201, 6, 65);
     padding: 5px;
   }
 }
@@ -135,28 +209,26 @@ nav > * {
   font-weight: bold;
 }
 @media all and (max-width: 621px) {
-  #entete img {
-    height: 100px;
+  #entete > img {
+    height: 50px;
     padding-left: 1px;
     padding-right: 1px;
-    width: 90px;
   }
-  nav > * {
+
+  
+  nav li {
     padding: 5px;
     text-align: center;
     font-weight: bold;
     font-size: 12px;
+    display: flex;
+    justify-content: center;
   }
   nav button {
     margin-left: 1px;
-    background-color: rgb(201, 6, 65);
     padding: 5px;
   }
 }
-@media all and (max-width: 444px) {
-  nav > * {
-    font-size: 10px;
-  }
-}
+
 </style>
 
